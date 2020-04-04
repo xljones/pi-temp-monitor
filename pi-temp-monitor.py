71545f33747d64f71282b9bccd430ccdafbf12b6
Xander Jones <alex.l.jones@me.com>
2020-04-04 17:16:57 +0100

    I can't spell F, added -o, --once option

--- pi-temp-monitor.py
+++ pi-temp-monitor.py
@@ -19,24 +19,29 @@ class TemperatureController:
     def __init__(self, argsin):
         args = argsin
 
-    def _get_temperature(self, faranheit):
+    def _get_temperature(self, fahrenheit):
         system_cmd = os.popen("vcgencmd measure_temp")
         temp_str = system_cmd.read()
         regex_pattern = re.compile("temp=([\d\.]+)'[CF]")
         regex_matches = regex_pattern.match(temp_str)
         self.current_temp = float(regex_matches.group(1))
-        if args.faranheit:
+        if args.fahrenheit:
             self.current_temp = (self.current_temp * (9/5)) + 32
-        return(self.current_temp)
+        return(round(self.current_temp,1))
 
     def _get_timestamp(self):
         now = datetime.datetime.now()
         return (now.strftime("%d/%m//%Y, %H:%M:%S"))
 
     def monitor_temperature(self):
-        while (True):
-            print("     {0}: {1}".format(self._get_timestamp(), self._get_temperature(args.faranheit)), end="\r")
-            time.sleep(1)
+        output = "     {0}: {1}*{2}         ".format(self._get_timestamp(), self._get_temperature(args.fahrenheit), "F" if args.fahrenheit else "C")
+        if args.once:
+            print(output)
+            print()
+        else:
+            while (True):
+                print(output, end="\r")
+                time.sleep(1)
 
 
 def print_header():
@@ -52,7 +57,7 @@ if (__name__ == "__main__"):
     # p.add_argument('-v', '--verbose', help='Turn on verbose logging', action='store_true')
     # p.add_argument('-r', '--record', help='Record data to file')
     p.add_argument('-o', '--once', help='Just show one instance of the temperature', action='store_true')
-    p.add_argument('-f', '--faranheit', help='Show temperature in degress Faranheit, default is to use degrees Celsius', action='store_true')
+    p.add_argument('-f', '--fahrenheit', help='Show temperature in degress Fahrenheit, default is to use degrees Celsius', action='store_true')
     args = p.parse_args()
 
     print_header()
