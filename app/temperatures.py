f49daf3c6fa210ac37597f100754a389841225e1
xljones <xljones@icloud.com>
2024-05-31 17:38:40 +0100

    tidy up, remove old graphing

--- app/temperatures.py
+++ app/temperatures.py
@@ -1,92 +1,107 @@
 from datetime import datetime
-import math
 import os
 import re
 import time
 
-from app.graphing import CursesBarGraph
+from typing import Any
 
 
 class TemperatureViewController:
     clock_speed = {
-        "setpoint": -999,
-        "current": -999,
-        "setpoint_last_update": datetime.now(),
+        "setpoint": -999.0,
+        "current": -999.0,
     }
-    temperature = {"current": -999}
+    temperature = {
+        "current": -999.0
+    }
 
-    def __init__(self, args, version):
+    def __init__(self, args: Any, version: str) -> None:
         self._args = args
         self._version = version
 
-    def _print_header(self) -> None:
-        print("")
-        print(f"Raspberry Pi Temperature Monitor v{self._version}")
-        print("")
-
-    def _get_temperature(self, fahrenheit: bool) -> float:
-        system_cmd = os.popen("vcgencmd measure_temp")
-        get_str = system_cmd.read()
-        regex_pattern = re.compile(r"temp=([\d\.]+)'[CF]")
-        regex_matches = regex_pattern.match(get_str)
-        self.temperature["current"] = float(regex_matches.group(1))
-        if self._args.fahrenheit:
-            self.temperature["current"] = (
-                self.temperature["current"] * (9 / 5)
-            ) + 32
-        return round(self.temperature["current"], 1)
-
-    def _get_clock_speed_setpoint(self) -> float:
-        # Returns clock speed in kHz
-        system_cmd = os.popen("vcgencmd get_config arm_freq")
-        get_str = system_cmd.read()
-        regex_pattern = re.compile(r"arm_freq=([\d\.]+)")
-        regex_matches = regex_pattern.match(get_str)
-        self.clock_speed["setpoint"] = (
-            float(regex_matches.group(1)) / 1000
-        )  # Command returns in MHz, /1000 convert to GHz
-        self.clock_speed["setpoint_last_update"] = datetime.now()
-        return round(self.clock_speed["setpoint"], 1)
-
-    def _get_clock_speed_current(self) -> float:
-        # Returns clock speed in Hz
-        system_cmd = os.popen("vcgencmd measure_clock arm")
-        get_str = system_cmd.read()
-        regex_pattern = re.compile(r"frequency\(\d+\)=([\d\.]+)")
-        regex_matches = regex_pattern.match(get_str)
-        self.clock_speed["current"] = (
-            float(regex_matches.group(1)) / 1000000000
-        )  # Command returns in Hz, /1000000 to convert to GHz
-        return round(self.clock_speed["current"], 1)
-
-    def _get_timestamp(self) -> str:
-        return datetime.now().strftime("%d/%m//%Y, %H:%M:%S")
-
     def graph_temperature(self) -> None:
-        with CursesBarGraph() as bar_graph:
-            while True:
-                values = []
-                for i in range(200):
-                    values.append(100 * (math.sin(0.05 + i / 20.0) + 1) / 2.0)
-                bar_graph.update(values)
+        raise NotImplementedError("Graphing is not implemented yet")
 
     def text_temperature(self) -> None:
+        OUTPUT = (
+            "{timestamp} (CPU {current_cpu_speed}"
+            "/{setpoint_cpu_speed}GHz): {temperature}     "
+        )
+
         self._print_header()
-        output = "{0} (CPU {1}/{2}GHz): {3}°{4}     "
         while True:
             print(
-                output.format(
-                    self._get_timestamp(),
-                    self._get_clock_speed_current(),
-                    self._get_clock_speed_setpoint(),
-                    self._get_temperature(self._args.fahrenheit),
-                    "F" if self._args.fahrenheit else "C",
+                OUTPUT.format(
+                    timestamp=self._get_timestamp(),
+                    current_cpu_speed=self._get_clock_speed_current(),
+                    setpoint_cpu_speed=self._get_clock_speed_setpoint(),
+                    temperature=self._format_temperature(
+                        temperature=self._get_temperature(),
+                    ),
                 ),
-                end="\r" if exit else "",
+                end="\r" if not self._args.once else "",
             )
             if self._args.once:
                 print("\n")
                 break
             else:
                 time.sleep(1)
-            
+
+    def _print_header(self) -> None:
+        print("")
+        print(f"Raspberry Pi Temperature Monitor v{self._version}")
+        print("")
+
+    @staticmethod
+    def _sys_cmd_and_regex(
+        command: str,
+        regex_pattern: str
+    ) -> str:
+        response = os.popen(command).read()
+        regex = re.compile(regex_pattern)
+        matches = regex.match(response)
+        assert matches
+        return matches.group(1)
+
+    def _get_temperature(self) -> float:
+        temperature_str = self._sys_cmd_and_regex(
+            command="vcgencmd measure_temp",
+            regex_pattern=r"temp=([\d\.]+)'[CF]",
+        )
+        self.temperature["current"] = float(temperature_str)
+        return round(self.temperature["current"], 1)
+
+    def _get_clock_speed_setpoint(self) -> float:
+        # Returns clock speed in kHz
+        clock_speed = self._sys_cmd_and_regex(
+            command="vcgencmd get_config arm_freq",
+            regex_pattern=r"arm_freq=([\d\.]+)",
+        )
+        self.clock_speed["setpoint"] = float(clock_speed) / 1_000
+        return round(self.clock_speed["setpoint"], 1)
+
+    def _get_clock_speed_current(self) -> float:
+        # Returns clock speed in Hz
+        clock_speed = self._sys_cmd_and_regex(
+            command="vcgencmd measure_clock arm",
+            regex_pattern=r"frequency\(\d+\)=([\d\.]+)",
+        )
+        self.clock_speed["current"] = float(clock_speed) / 1_000_000_000
+        return round(self.clock_speed["current"], 1)
+
+    def _format_temperature(
+        self,
+        temperature: float,
+    ) -> str:
+        TEMPLATE = "{temperature}°{unit}"
+
+        if self._args.fahrenheit:
+            return TEMPLATE.format(
+                temperature=(temperature * (9 / 5)) + 32,
+                unit="F",
+            )
+
+        return TEMPLATE.format(temperature=temperature, unit="C")
+
+    def _get_timestamp(self) -> str:
+        return datetime.now().strftime("%d/%m//%Y, %H:%M:%S")
